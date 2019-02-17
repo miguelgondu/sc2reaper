6785f4227102dd3ffc2c6174a57eaa19edbaf29f
Oscar Arbeláez <odarbelaeze@gmail.com>
2019-02-17 09:43:01 +0100

    Work on the api

--- sc2reaper/cli.py
+++ sc2reaper/cli.py
@@ -5,13 +5,22 @@ import sys
 import click
 
 
-@click.command()
+@click.group()
 def main(args=None):
-    """Console script for sc2reaper."""
-    click.echo("Replace this message by putting your code into "
-               "sc2reaper.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+    """SC2 Reaper command line tools"""
     return 0
+
+
+@main.command()
+@click.argument('files', nargs=-1, type=click.File('r'))
+@click.option('--mongo-url', '-m', type=str, default=None, help="MongoDB URL.")
+def ingest(files, mongo_url):
+    """
+    Load a few replays into a mongo database.
+    """
+    click.echo(f'Storing replays in {mongo_url}')
+    for _file in files:
+        click.echo(_file.name)
 
 
 if __name__ == "__main__":
--- tests/test_sc2reaper.py
+++ tests/test_sc2reaper.py
@@ -11,28 +11,12 @@ from sc2reaper import sc2reaper
 from sc2reaper import cli
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-
 def test_command_line_interface():
     """Test the CLI."""
     runner = CliRunner()
     result = runner.invoke(cli.main)
     assert result.exit_code == 0
-    assert 'sc2reaper.cli.main' in result.output
+    assert 'SC2 Reaper command line tools' in result.output
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
