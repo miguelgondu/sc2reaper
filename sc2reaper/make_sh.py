850a98827fd306aaeab49a7e58a62b7da27d19e1
Miguel González <miguelgondu@gmail.com>
2019-02-17 09:54:04 +0100

    Adds the real deal

---
+++ sc2reaper/make_sh.py
@@ -0,0 +1,13 @@
+from glob import glob
+
+def main():
+    files = glob("/home/mgd/StarCraftII/Replays/*.SC2Replay")
+    script = "replay_parser.py"
+    print(f"files: {files}")
+
+    with open("to_run.sh", mode="w") as to_run:
+        for f in files[:10]:
+            to_run.write(f"python {script} -replay={f}\n")
+
+if __name__ == '__main__':
+    main()
