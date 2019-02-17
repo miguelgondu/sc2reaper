7682e559617630ef59d3fa9353f5026cca596367
Oscar Arbeláez <odarbelaeze@gmail.com>
2019-02-17 10:01:32 +0100

    Refactor encoder

--- sc2reaper/action_extraction.py
+++ sc2reaper/action_extraction.py
@@ -1,9 +1,11 @@
 from encoder import encoder
+
 enc = encoder()
-e = lambda x: 'e' + str(enc[x])
+e = lambda x: "e" + str(enc[x])
+
 
 def is_macro_action(unit_command_action, abilities):
-	'''
+    """
 	This function determines if a unit_command action is a macro action
 	based on the following criteria: if the action is related to training, morphing,
 	researching or building units or technologies, then it is a macro action. It 
@@ -11,33 +13,35 @@ def is_macro_action(unit_command_action, abilities):
 
 	Feel free to add more stuff in macro_names by checking the stableid.json and
 	adding names that you feel should be stored as macro actions.
-	'''
-	macro_names = [
-		"Morph",
-		"Research",
-		"Train",
-		"ZergBuild",
-		"ProtossBuild",
-		"TerranBuild",
-	]
+	"""
+    macro_names = [
+        "Morph",
+        "Research",
+        "Train",
+        "ZergBuild",
+        "ProtossBuild",
+        "TerranBuild",
+    ]
 
-	for name in macro_names:
-		ability = abilities[unit_command_action.ability_id]
-		# print(f"ability: {ability}")
-		if name in ability.link_name:
-			# we're also storing the the canceling of stuff. 
-			# if hasattr(ability, 'button_name') and 'Cancel' in ability.button_name:
-			# 	return False 
-			return True
+    for name in macro_names:
+        ability = abilities[unit_command_action.ability_id]
+        # print(f"ability: {ability}")
+        if name in ability.link_name:
+            # we're also storing the the canceling of stuff.
+            # if hasattr(ability, 'button_name') and 'Cancel' in ability.button_name:
+            # 	return False
+            return True
 
-	return False
+    return False
+
 
 def get_human_name(action_doc, abilities):
-	ability = abilities[action_doc["ability_id"]]
-	return str(ability.link_name) + str(ability.button_name)
+    ability = abilities[action_doc["ability_id"]]
+    return str(ability.link_name) + str(ability.button_name)
+
 
 def get_actions(obs, abilities):
-	'''
+    """
 	This function is supposed to return macro actions from an observation.
 
 	Arguments:
@@ -52,29 +56,35 @@ def get_actions(obs, abilities):
 
 	TO-DO:
 		- clean the hasattr, it seems to be innecesary
-	'''
+	"""
 
-	actions = obs.actions
-	# print(f"actions in frame {obs.observation.game_loop}:")
-	# print(actions)
-	macro_actions = []
-	for action in actions:
-		if hasattr(action, "action_raw") and hasattr(action.action_raw, "unit_command"):
-			if is_macro_action(action.action_raw.unit_command, abilities):
-				a = action.action_raw.unit_command
-				# print(f"action {a} is macro! ({abilities[a.ability_id].button_name})")
-				action_doc = {}
-				if hasattr(action.action_raw.unit_command, "ability_id"):
-					action_doc[e("ability_id")] = action.action_raw.unit_command.ability_id
-				if hasattr(action.action_raw.unit_command, "unit_tags"):
-					action_doc[e("unit_tags")] = [tag for tag in action.action_raw.unit_command.unit_tags]
-				if hasattr(action.action_raw.unit_command, "target_unit_tag"):
-					action_doc[e("target_unit_tag")] = action.action_raw.unit_command.target_unit_tag
-				if hasattr(action.action_raw.unit_command, "target_world_space_pos"):
-					action_doc[e("target_world_space_pos")] = {
-						"x": action.action_raw.unit_command.target_world_space_pos.x,
-						"y": action.action_raw.unit_command.target_world_space_pos.y
-				}
-				macro_actions.append(action_doc)
-	
-	return macro_actions
+    actions = obs.actions
+    # print(f"actions in frame {obs.observation.game_loop}:")
+    # print(actions)
+    macro_actions = []
+    for action in actions:
+        if hasattr(action, "action_raw") and hasattr(action.action_raw, "unit_command"):
+            if is_macro_action(action.action_raw.unit_command, abilities):
+                a = action.action_raw.unit_command
+                # print(f"action {a} is macro! ({abilities[a.ability_id].button_name})")
+                action_doc = {}
+                if hasattr(action.action_raw.unit_command, "ability_id"):
+                    action_doc[
+                        e("ability_id")
+                    ] = action.action_raw.unit_command.ability_id
+                if hasattr(action.action_raw.unit_command, "unit_tags"):
+                    action_doc[e("unit_tags")] = [
+                        tag for tag in action.action_raw.unit_command.unit_tags
+                    ]
+                if hasattr(action.action_raw.unit_command, "target_unit_tag"):
+                    action_doc[
+                        e("target_unit_tag")
+                    ] = action.action_raw.unit_command.target_unit_tag
+                if hasattr(action.action_raw.unit_command, "target_world_space_pos"):
+                    action_doc[e("target_world_space_pos")] = {
+                        "x": action.action_raw.unit_command.target_world_space_pos.x,
+                        "y": action.action_raw.unit_command.target_world_space_pos.y,
+                    }
+                macro_actions.append(action_doc)
+    return macro_actions
+
