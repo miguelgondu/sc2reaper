a2b2a24b88c2dee5e9cbe34bc0933e77ce1713ec
Miguel González <miguelgondu@gmail.com>
2019-02-17 20:29:29 +0100

    Deploying on server, wish me luck

--- sc2reaper/make_sh.py
+++ sc2reaper/make_sh.py
@@ -2,11 +2,10 @@ from glob import glob
 
 def main():
     files = glob("/mnt/disk/miguel/StarCraftII/Replays/*.SC2Replay")
-    script = "replay_parser.py"
-    print(f"files: {files}")
+    # print(f"files: {files}")
 
     with open("to_run.sh", mode="w") as to_run:
-        for f in files[:10]:
+        for f in files[:30000]:
             to_run.write(f"sc2reaper ingest {f}\n")
 
 if __name__ == '__main__':
--- sc2reaper/mongo_interface.py
+++ sc2reaper/mongo_interface.py
@@ -2,7 +2,15 @@ from pymongo import MongoClient
 
 client = MongoClient()
 db = client.replays_database
+
+for coll in db.list_collections():
+	print(coll)
+
 replays = db.replays
+players = db.players
+
+for player in players.find({'replay_id': "d2985a7db946b471be9ec8fea352f407b7a7ef9b2e42e5ecf60d14b695649e61"}):
+	print(player)
 
 for replay in replays.find():
 	print(replay["replay_id"], replay["match_up"])
