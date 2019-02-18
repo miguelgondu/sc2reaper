3d5b4c5630d428f299483d48e702e65a3ecc5e88
Miguel González <miguelgondu@gmail.com>
2019-02-18 15:19:52 +0100

    pre-merge

--- sc2reaper/mongo_interface.py
+++ sc2reaper/mongo_interface.py
@@ -11,10 +11,12 @@ players = db.players
 
 for player in players.find({'replay_id': "d2985a7db946b471be9ec8fea352f407b7a7ef9b2e42e5ecf60d14b695649e61"}):
 	print(player)
-
+counter = 0
 for replay in replays.find():
+	counter += 1
 	print(replay["replay_id"], replay["match_up"])
 
+print(f"amount of replays stored: {counter}")
 # for state in states.find({'replay_name': "/home/mgd/StarCraftII/Replays/3f9e30f72c11d74d5616d813ee040de28e41269280b658b61af0e48211c8f43c.SC2Replay", "player_id": 1}, {"supply":1, "frame_id":1}).sort("frame_id", 1): 
 # 	print() 
 #     print(state)   
